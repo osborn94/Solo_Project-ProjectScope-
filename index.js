f5ef42a69498e8598b5bd663aa49bdf5c8c2ca6a
osborn israel <israelosborn94@yahoo.com>
2024-08-19 22:42:10 +0100

    reworked on the index page responsiveness

---
+++ index.js
@@ -0,0 +1,13 @@
+// menu button
+const menuBtn = document.querySelector('.menuIcon')
+        const navlinks = document.querySelector('.nav-links')
+        menuBtn.addEventListener('click',()=>{
+            navlinks.classList.toggle('mobile-menu')
+        })
+// scroll to Blog section
+function scrollToClass(className) {
+    const element = document.querySelector(`.${className}`);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+}
